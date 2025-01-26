ee3cf5b0938fac5573cddd20a26d7baec561063f
Yussuf Faisal <thecrusheda@gmail.com>
2025-01-26 11:16:33 +0700

    update Beranda part 2.3

--- resources/views/pages/portal/index.blade.php
+++ resources/views/pages/portal/index.blade.php
@@ -169,12 +169,20 @@
                     <img src="{{ asset('/img/penunjang/ambulance1.png') }}" width="600" style="max-width: 100%" alt="">
                 </div>
                 <div class="col-md-6">
-                    <h2 class="fs-15 text-uppercase text-muted mb-3">Instalasi Gawat Darurat</h2>
-                    <h3 class="display-3 ls-sm mb-5">Pelayanan Gawat Darurat<br><b class="text-danger">24 Jam</b></h3>
+                    {{-- <h2 class="fs-15 text-uppercase text-muted mb-3">Instalasi Gawat Darurat</h2> --}}
                     <div class="d-flex flex-row mb-5">
-                        <p class="mb-0">Rumah Sakit PKU Muhammadiyah Sukoharjo siap menghadapi kondisi apapun dalam waktu kapan saja.
-                            Rumah Sakit PKU Muhammadiyah Sukoharjo memiliki <strong>Tim Code Blue</strong> yang merupakan tim gawat darurat
-                            intra hospital dan merupakan pelayanan emergency yang diberikan Rumah Sakit PKU Muhammadiyah Sukoharjo.</p>
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 507 512" data-inject-url="https://sandbox.elemisthemes.com/assets/img/icons/lineal/telephone-3.svg" class="svg-inject icon-svg icon-svg-md text-blue me-5 mt-1"><path class="lineal-fill" d="M402.4 437.9c0-10.5-4.4-20.4-12.6-28.7-5.1-5.6-10.6-11-16-16.1-2.7-2.6-5.5-5.3-8.1-8l-23.4-23.4c-18-18-41.3-18-59.4 0-2.4 2.4-4.9 4.8-7.3 7.2-6.4 6.4-13.1 12.9-19.6 19.9-11.6-5.2-23-12.4-36.5-23.2-13.4-10.9-62.2-59.7-73.2-73.2-10.7-13.5-17.9-25-23.2-36.5 7-6.4 13.5-13.1 19.9-19.6 2.4-2.4 4.8-4.9 7.2-7.3 18-18 18-41.3 0-59.4l-23.4-23.4c-2.7-2.7-5.4-5.4-8-8.1-5.2-5.3-10.6-10.8-16.1-16-8.3-8.2-18.3-12.6-28.7-12.6s-20.5 4.5-29.1 12.7l-.2.2L33.4 134c-10.8 10.6-17.4 24.8-18.6 39.9-2.1 25 5.3 48.4 11 63.6C39.6 275 60.4 309.7 91.3 347c11.2 13.3 23.2 26 35.9 37.8 11.8 12.7 24.4 24.7 37.8 35.8 37.2 31 72 51.7 109.5 65.6 15.3 5.7 38.6 13 63.6 11 15.1-1.2 29.3-7.8 39.9-18.6l11.6-11.4.2-.2c8.2-8.5 12.6-18.6 12.6-29.1z"></path><path class="lineal-stroke" d="M507 237.1C507 106.4 400.7 0 270 0c-54.9 0-108.2 19.1-150.6 54-6.1 5.1-7 14.2-1.9 20.3 5.1 6.1 14.1 7 20.2 2 37.3-30.6 84-47.4 132.3-47.4 114.9 0 208.3 93.4 208.3 208.3-.1 63.1-28.7 122.8-77.8 162.4l-.2-.2c-5.4-5.8-11.1-11.4-16.4-16.5l-.2-.1c-2.5-2.3-5-4.8-7.4-7.3l-23.8-23.8c-11.6-11.6-25.4-17.7-39.9-17.7s-28.3 6.1-39.9 17.7l-3.4 3.4c-1.1 1.1-2.6 2.5-3.8 3.7l-1.8 1.7c-3.5 3.5-7.1 7-10.8 10.7-8.6-4.9-16.8-10.5-24.4-16.7-12.9-10.5-60.4-58.1-71-71-6.2-7.7-11.8-15.8-16.8-24.4 3.7-3.6 7.2-7.2 10.7-10.7l1.7-1.8c1.3-1.3 2.6-2.6 3.8-3.9 1.1-1.1 2.2-2.3 3.4-3.4 23.5-23.5 23.5-56.2 0-79.7L149 148.3c4.5-6.1 9.4-11.8 14.8-17.1 58.5-58.5 153.6-58.5 212 0 44.9 45 56.6 113.4 29.3 170.8-3.4 7.2-.4 15.8 6.8 19.2s15.8.4 19.2-6.8c32.6-68.4 18.7-149.9-34.9-203.5-69.7-69.7-183.1-69.7-252.7 0-5.3 5.3-10.3 10.9-14.9 16.8-5-5.2-10.3-10.6-16-15.8-10.9-11-24.3-16.7-38.6-16.7s-27.8 5.7-39.1 16.6l-.2.2-.2.2-.1.1-11.3 11.6C9.9 136.9 1.8 154.2.4 172.7c-2.3 27.8 5.7 53.2 11.8 69.8 14.5 39.2 36.1 75.3 68.1 113.7C91.7 369.9 104 382.8 117 395c12.2 13 25.1 25.3 38.8 36.8 38.4 31.9 74.5 53.5 113.6 68 14.2 5.2 35.8 12.2 60 12.2 3.3 0 6.7-.1 10-.4 18.5-1.5 35.8-9.5 48.7-22.7l11.6-11.3.1-.1.2-.2.2-.2c10.9-11.3 16.6-24.9 16.6-39.1 0-4.5-.6-9-1.8-13.4 58-45 91.9-114.2 92-187.5zM379.5 457L368 468.4l-.1.1c-8.2 8.4-19.2 13.6-30.9 14.4-2.5.2-5 .3-7.5.3-19.5 0-37.8-6-49.9-10.4-36-13.4-69.5-33.4-105.2-63.1-12.9-10.8-25.1-22.3-36.6-34.6-.2-.3-.5-.5-.7-.7-12.3-11.4-23.8-23.6-34.6-36.5-29.8-35.9-49.9-69.4-63.3-105.4-5.2-14.1-11.9-35.3-10.1-57.4.9-11.8 6-22.8 14.5-31l.1-.1L55 132.5c5.8-5.6 12.4-8.5 19.1-8.5s13 2.9 18.6 8.4l.4.4c5.3 4.9 10.6 10.3 15.5 15.3 2.6 2.7 5.2 5.4 7.8 8l23.8 23.8c12.4 12.4 12.4 26.6 0 39l-3.6 3.6c-1.2 1.2-2.4 2.5-3.7 3.8l-1.9 1.8c-5.6 5.7-11.5 11.6-17.6 17.2-4.6 4.2-6 10.9-3.4 16.5 5.7 12.7 13.7 25.2 25 39.5l.1.2c11.7 14.4 60.9 63.6 75.3 75.3l.1.1c14.3 11.3 26.9 19.3 39.5 25 5.7 2.6 12.3 1.2 16.5-3.4 5.6-6.1 11.5-12 17.3-17.6l1.9-1.8c1.2-1.2 2.4-2.4 3.7-3.6s2.4-2.4 3.6-3.6c6.1-6.1 12.8-9.3 19.5-9.3s13.4 3.2 19.5 9.3l23.8 23.8c2.6 2.6 5.3 5.2 8 7.8 5 4.9 10.4 10.1 15.3 15.5l.4.4c3.9 3.9 8.4 10.3 8.4 18.6.1 6.6-2.8 13.2-8.4 19z"></path><path class="lineal-stroke" d="M274.8 258.4h-36.5c.1-4.4 6.9-9 14.7-14.3 13.4-9.2 30-20.6 30-40.4 0-20.4-18.1-31.4-35.1-31.4-16.6 0-34.1 10.2-34.1 29.2 0 8.7 3.9 13.2 11.6 13.2 8.6 0 13.3-5 13.3-9.7 0-7.8 5.1-9.4 9.4-9.4 7.1 0 9.6 4.8 9.6 8.8 0 8-9.9 15.2-20.4 22.9-12.4 9.1-25.2 18.5-25.2 31.2v14c0 5.2 6.6 8.4 11.3 8.4h51.5c4.4 0 8.2-5.3 8.2-11.4s-3.8-11.1-8.3-11.1z"></path><path class="lineal-stroke" d="M351.9 234.9h-4.8v-8.7c0-5-5-8.2-12.7-8.2s-12.7 3.2-12.7 8.2v8.7H313l24.6-49.3c.5-1 .8-2.2.8-3.3 0-6.2-8.2-10-12.7-10-4.6-.1-8.8 2.5-10.7 6.6l-33.4 65.5c-.8 1.5-1.3 3.2-1.3 5 0 5.5 3.6 9.3 9 9.3h32.4v13.9c0 5.8 6.6 8.4 12.7 8.4s12.7-2.6 12.7-8.4v-13.9h4.8c4.3 0 8.4-5.8 8.4-11.9 0-5.7-2.6-11.9-8.4-11.9z"></path></svg>
+                            {{-- <img src="{{ asset('img/icons/lineal/megaphone.svg') }}"
+                                class="svg-inject icon-svg icon-svg-md text-purple me-5 mt-1" alt="" /> --}}
+                        </div>
+                        <div>
+                            <h4 class="">Pelayanan Gawat Darurat</h4>
+                            <h3 class="display-3 ls-sm mb-3"><b class="text-danger">IGD 24 Jam</b></h3>
+                            <p class="mb-0">Rumah Sakit PKU Muhammadiyah Sukoharjo siap menghadapi kondisi apapun dalam waktu kapan saja.
+                                Rumah Sakit PKU Muhammadiyah Sukoharjo memiliki <strong>Tim Code Blue</strong> yang merupakan tim gawat darurat
+                                intra hospital dan merupakan pelayanan emergency yang diberikan Rumah Sakit PKU Muhammadiyah Sukoharjo.</p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -211,7 +219,7 @@
     </section>
     <!-- /section -->
     <section class="wrapper bg-white">
-        <div class="container py-5 py-md-10">
+        <div class="container py-14 pt-md-16 pt-lg-16 pb-md-16 py-md-10">
             <div class="row text-center">
                 <div class="col-lg-10 col-xl-7 col-xxl-6 mx-auto">
                     <h2 class="fs-15 text-uppercase text-muted mb-3">E-Poster</h2>
@@ -388,7 +396,7 @@
     </section>
 
     <section class="wrapper bg-white">
-        <div class="container py-5 py-md-10">
+        <div class="container py-14 pt-md-16 pt-lg-16 pb-md-16 py-md-10">
             <div class="row text-center">
                 <div class="col-lg-10 col-xl-7 col-xxl-6 mx-auto">
                     <h2 class="fs-15 text-uppercase text-muted mb-3">Lini Masa</h2>
@@ -517,6 +525,141 @@
         </div>
         <!-- /.container -->
     </section>
+
+    <section class="wrapper bg-light">
+        <div class="container pb-14 pb-md-14 mb-lg-18 mb-xl-18">
+          <!-- /.row -->
+          <div class="row gx-lg-8 gx-xl-12 gy-10 align-items-center mb-14 mb-md-18">
+            <div class="col-lg-6 position-relative">
+              <div class="shape rounded bg-pale-red rellax d-block" data-rellax-speed="0" style="top: 50%; left: 50%; width: 50%; height: 60%; transform: translate(-50%,-50%);z-index:0"></div>
+              <div class="row gx-md-5 gy-5 position-relative">
+                <div class="col-6">
+                  <img class="img-fluid rounded shadow-lg mb-5" data-cue="fadeIn" data-delay="300" src="{{ asset('img/photos/sa12.jpg') }}" srcset="{{ asset('img/photos/sa1@2x.jpg') }} 2x" alt="" />
+                  <img class="img-fluid rounded shadow-lg d-flex col-10 ms-auto" data-cue="fadeIn" data-delay="600" src="{{ asset('img/photos/sa12.jpg') }}" srcset="{{ asset('img/photos/sa1@2x.jpg') }} 2x" alt="" />
+                </div>
+                <!-- /column -->
+                <div class="col-6">
+                  <img class="img-fluid rounded shadow-lg my-5" data-cue="fadeIn" data-delay="900" src="{{ asset('img/photos/sa12.jpg') }}" srcset="{{ asset('img/photos/sa1@2x.jpg') }} 2x" alt="" />
+                  <img class="img-fluid rounded shadow-lg d-flex col-10" data-cue="fadeIn" data-delay="1200" src="{{ asset('img/photos/sa12.jpg') }}" srcset="{{ asset('img/photos/sa1@2x.jpg') }} 2x" alt="" />
+                </div>
+                <!-- /column -->
+              </div>
+              <!-- /.row -->
+            </div>
+            <!--/column -->
+            <div class="col-lg-6">
+              <h3 class="display-4 mb-5">We have considered our solutions to support every stage of growth.</h3>
+              <p class="mb-5">Etiam porta sem malesuada magna mollis euismod. Donec ullamcorper nulla non metus auctor fringilla. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Fusce dapibus, tellus ac cursus commodo.</p>
+              <div class="row gy-3">
+                <div class="col-xl-6">
+                  <ul class="icon-list bullet-bg bullet-soft-red mb-0">
+                    <li><span><i class="uil uil-check"></i></span><span>Aenean quam ornare curabitur blandit consectetur.</span></li>
+                    <li class="mt-3"><span><i class="uil uil-check"></i></span><span>Nullam quis risus eget urna mollis ornare aenean leo.</span></li>
+                  </ul>
+                </div>
+                <!--/column -->
+                <div class="col-xl-6">
+                  <ul class="icon-list bullet-bg bullet-soft-red mb-0">
+                    <li><span><i class="uil uil-check"></i></span><span>Etiam porta euismod malesuada mollis nisl ornare.</span></li>
+                    <li class="mt-3"><span><i class="uil uil-check"></i></span><span>Vivamus sagittis lacus augue rutrum maecenas odio.</span></li>
+                  </ul>
+                </div>
+                <!--/column -->
+              </div>
+              <!--/.row -->
+            </div>
+            <!--/column -->
+          </div>
+          <!--/.row -->
+          <div class="row gx-lg-8 gx-xl-12 gy-10 align-items-center mb-14 mb-md-18">
+            <div class="col-lg-6 position-relative order-lg-2">
+              <div class="shape rounded bg-pale-green rellax d-block" data-rellax-speed="0" style="top: 50%; left: 50%; width: 50%; height: 60%; transform: translate(-50%,-50%);z-index:0"></div>
+              <div class="row gx-md-5 gy-5 position-relative">
+                <div class="col-5">
+                  <img class="img-fluid rounded shadow-lg my-5 d-flex ms-auto" data-cue="fadeIn" data-delay="300" src="{{ asset('img/photos/sa12.jpg') }}" srcset="{{ asset('img/photos/sa1@2x.jpg') }} 2x" alt="" />
+                  <img class="img-fluid rounded shadow-lg d-flex col-10 ms-auto" data-cue="fadeIn" data-delay="600" src="{{ asset('img/photos/sa12.jpg') }}" srcset="{{ asset('img/photos/sa1@2x.jpg') }} 2x" alt="" />
+                </div>
+                <!-- /column -->
+                <div class="col-7">
+                  <img class="img-fluid rounded shadow-lg mb-5" data-cue="fadeIn" data-delay="900" src="{{ asset('img/photos/sa12.jpg') }}" srcset="{{ asset('img/photos/sa1@2x.jpg') }} 2x" alt="" />
+                  <img class="img-fluid rounded shadow-lg d-flex col-11" data-cue="fadeIn" data-delay="1200" src="{{ asset('img/photos/sa12.jpg') }}" srcset="{{ asset('img/photos/sa1@2x.jpg') }} 2x" alt="" />
+                </div>
+                <!-- /column -->
+              </div>
+              <!-- /.row -->
+            </div>
+            <!--/column -->
+            <div class="col-lg-6">
+              <h3 class="display-4 mb-5">We make spending stress free so you have the perfect control.</h3>
+              <p class="mb-5">Etiam porta sem malesuada magna mollis euismod. Donec ullamcorper nulla non metus auctor fringilla. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Fusce dapibus, tellus ac cursus commodo.</p>
+              <div class="row gy-3">
+                <div class="col-xl-6">
+                  <ul class="icon-list bullet-bg bullet-soft-green mb-0">
+                    <li><span><i class="uil uil-check"></i></span><span>Aenean quam ornare curabitur blandit consectetur.</span></li>
+                    <li class="mt-3"><span><i class="uil uil-check"></i></span><span>Nullam quis risus eget urna mollis ornare aenean leo.</span></li>
+                  </ul>
+                </div>
+                <!--/column -->
+                <div class="col-xl-6">
+                  <ul class="icon-list bullet-bg bullet-soft-green mb-0">
+                    <li><span><i class="uil uil-check"></i></span><span>Etiam porta euismod malesuada mollis nisl ornare.</span></li>
+                    <li class="mt-3"><span><i class="uil uil-check"></i></span><span>Vivamus sagittis lacus augue rutrum maecenas odio.</span></li>
+                  </ul>
+                </div>
+                <!--/column -->
+              </div>
+              <!--/.row -->
+            </div>
+            <!--/column -->
+          </div>
+        </div>
+        <!-- /.container -->
+      </section>
+
+    <section class="wrapper bg-soft-primary">
+        <div class="container py-14 pt-md-16 pt-lg-0 pb-md-16">
+          <div class="row text-center" data-cue="slideInUp">
+            <div class="col-lg-10 mx-auto">
+              <div class="mt-lg-n20 mt-xl-n22 position-relative">
+                <div class="shape bg-dot red rellax w-16 h-18" data-rellax-speed="1" style="top: 1rem; left: -3.9rem;"></div>
+                <div class="shape rounded-circle bg-line primary rellax w-18 h-18" data-rellax-speed="1" style="bottom: 2rem; right: -3rem;"></div>
+                <video poster="{{ asset('media/thumbnail.jpg') }}" class="player" playsinline controls preload="none">
+                  <source src="{{ asset('media/profil.mp4') }}" type="video/mp4">
+                  {{-- <source src="{{ asset('media/movie.html') }}" type="video/webm"> --}}
+                </video>
+              </div>
+            </div>
+            <!--/column -->
+          </div>
+          <!--/.row -->
+          <div class="row text-center mt-12">
+            <div class="col-lg-9 mx-auto">
+              <h3 class="display-4 mb-0 text-center px-xl-10 px-xxl-15">Find out everything you need to know about creating a business process model</h3>
+              <div class="row gx-lg-8 gx-xl-12 process-wrapper text-center mt-9" data-cues="slideInUp" data-group="process">
+                <div class="col-md-4"> <img src="{{ asset('img/icons/lineal/shield.svg') }}" class="svg-inject icon-svg icon-svg-md text-red mb-3" alt="" />
+                  <h4 class="mb-1">1. Secured Transactions</h4>
+                  <p>Etiam porta malesuada magna mollis euismod sem.</p>
+                </div>
+                <!--/column -->
+                <div class="col-md-4"> <img src="{{ asset('img/icons/lineal/shield.svg') }}" class="svg-inject icon-svg icon-svg-md text-green mb-3" alt="" />
+                  <h4 class="mb-1">2. Budget Planning</h4>
+                  <p>Etiam porta malesuada magna mollis euismod sem.</p>
+                </div>
+                <!--/column -->
+                <div class="col-md-4"> <img src="{{ asset('img/icons/lineal/shield.svg') }}" class="svg-inject icon-svg icon-svg-md text-yellow mb-3" alt="" />
+                  <h4 class="mb-1">3. Up to Date</h4>
+                  <p>Etiam porta malesuada magna mollis euismod sem.</p>
+                </div>
+                <!--/column -->
+              </div>
+              <!--/.row -->
+            </div>
+            <!-- /column -->
+          </div>
+          <!-- /.row -->
+        </div>
+        <!-- /.container -->
+      </section>
     <!-- /section -->
     <section class="wrapper bg-white">
         <div class="container py-5 py-md-10">
@@ -629,13 +772,11 @@
                                 <div class="col-lg-8 ps-xl-10">
                                     <span class="ratings five fs-20 mb-3"></span>
                                     <blockquote class="border-0 fs-lg mb-0">
-                                        <p>“Donec id elit non mi porta gravida at eget metus. Vivamus mollis est non
-                                            commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec
-                                            elit. Duis mollis porta est non commodo luctus.”</p>
+                                        <p>“Kesembuhan Datangnya dari Allah, <br>
+                                            Kepuasan Anda adalah tanggungjawab kami”</p>
                                         <div class="blockquote-details justify-content-center text-center">
                                             <div class="info p-0">
-                                                <h4 class="ls-sm mb-1">Coriss Ambady</h4>
-                                                <p class="mb-0">Financial Analyst</p>
+                                                <h4 class="ls-sm mb-1">Motto Kami</h4>
                                             </div>
                                         </div>
                                     </blockquote>
